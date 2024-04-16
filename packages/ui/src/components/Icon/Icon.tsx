8f4b2a6822d2746a14a6755846825638e6b9d422
Shiyamal-Kumar <shiyamalkumar.asokan@bounteous.com>
2024-04-16 23:25:58 +0530

    added icon component

--- packages/ui/src/components/Icon/Icon.tsx
+++ packages/ui/src/components/Icon/Icon.tsx
@@ -1,3 +1,62 @@
-export function Icon() {
-  return <div>Icon</div>;
+import {
+  FaHeart, // for like
+  FaUser, // for user profile
+  FaBell, // for notification
+  FaEllipsisH, // for kebab menu (vertical dots)
+  FaSearch, // for search or magnifying glass
+  FaUserPlus, // for add user
+  FaTrophy, // for trophy
+  FaTshirt, // for dress hanger (closest match)
+  FaComments, // for chatting or messaging
+  FaExternalLinkSquareAlt, // for link icon (square with up arrow)
+  FaEnvelopeOpenText, // for letter icon
+  FaShoppingBasket, // for basket icon
+  FaPhone, // for call icon
+  FaSms, // for message icon (SMS)
+  FaEnvelope, // for email icon
+  FaInfoCircle, // for info icon
+  FaEye, // for view password eye
+  FaBirthdayCake, // for birthday cake
+  FaAward, // for loyalty badge
+  FaEdit,
+} from "react-icons/fa";
+
+const iconMap = {
+  heart: FaHeart,
+  user: FaUser,
+  notification: FaBell,
+  kebabMenu: FaEllipsisH,
+  search: FaSearch,
+  addUser: FaUserPlus,
+  trophy: FaTrophy,
+  dressHanger: FaTshirt,
+  chatting: FaComments,
+  linkIcon: FaExternalLinkSquareAlt,
+  letter: FaEnvelopeOpenText,
+  basket: FaShoppingBasket,
+  call: FaPhone,
+  message: FaSms,
+  email: FaEnvelope,
+  info: FaInfoCircle,
+  viewPassword: FaEye,
+  birthdayCake: FaBirthdayCake,
+  loyaltyBadge: FaAward,
+  edit: FaEdit,
+};
+
+interface IconProps {
+  iconName: keyof typeof iconMap;
+  size?: string | number;
+  color?: string;
+  className?: string;
 }
+
+export const Icon = ({
+  iconName,
+  size = "1em",
+  color = "black",
+  className = "",
+}: IconProps) => {
+  const IconComponent = iconMap[iconName];
+  return <IconComponent size={size} color={color} className={className} />;
+};
