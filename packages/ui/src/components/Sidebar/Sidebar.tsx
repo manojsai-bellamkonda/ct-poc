1ff4e75d2ba2203eb261784086f942ac19e0b077
Manoj Sai Bellamkonda <manojsai.bellamkonda@bounteous.com>
2024-04-19 01:40:05 +0530

    made sidebar scrollble and changed background colour.

--- packages/ui/src/components/Sidebar/Sidebar.tsx
+++ packages/ui/src/components/Sidebar/Sidebar.tsx
@@ -5,16 +5,15 @@ import { Button } from "@repo/ui";
 
 export function Sidebar() {
   return (
-    <div className="flex flex-col max-w-5xl text-sm lg:flex w-80 min-h-screen items-stretch shrink-0 text-black">
-      <div className="flex flex-col grow items-stretch bg-gradient-to-b from-white via-white to-gray-300">
-        <div className="flex pt-8">
-          <img
-            src="/default-pic.png"
-            className="h-5 w-[88px] mx-10 my-4 object-cover border-2"
-          />
-        </div>
-        <div className="flex font-semibold mx-10">#39 Water Tower Place</div>
-
+    <div className="flex flex-col max-w-5xl text-sm w-80 h-screen items-stretch text-black grow bg-gradient-to-br from-ghost-white  to-white-smoke">
+      <div className="flex pt-8">
+        <img
+          src="/default-pic.png"
+          className="h-5 w-[88px] mx-10 my-4 object-cover border-2"
+        />
+      </div>
+      <div className="font-semibold mx-10">#39 Water Tower Place</div>
+      <div className="flex-1 overflow-scroll">
         <div className="py-4 pt-16">
           <SideHeader active={true} iconName="letter">
             Dashboard
@@ -28,7 +27,6 @@ export function Sidebar() {
           <SideItem>Newly Assigned</SideItem>
           <SideItem>Recently Active</SideItem>
         </div>
-
         <div className="py-4">
           <SideHeader iconName="dressHanger">Products</SideHeader>
           <SideItem>New Arrivals</SideItem>
@@ -38,20 +36,19 @@ export function Sidebar() {
           <SideItem>Work Wear</SideItem>
           <SideItem>Shoes & Accessories</SideItem>
         </div>
-        <div className="grow"></div>
-        <div className="p-8 flex flex-col gap-5">
-          <div className="flex items-center gap-3 font-semibold">
-            <div>
-              <img
-                className="h-12 w-12 rounded-full object-cover"
-                src="profile-pic.png"
-                alt="lady"
-              />
-            </div>
-            <div>Sylvia #33457</div>
+      </div>
+      <div className="p-8 flex flex-col gap-5">
+        <div className="flex items-center gap-3 font-semibold">
+          <div>
+            <img
+              className="h-12 w-12 rounded-full object-cover"
+              src="profile-pic.png"
+              alt="lady"
+            />
           </div>
-          <Button label="Log Out" buttonStyle="px-12"></Button>
+          <div>Sylvia #33457</div>
         </div>
+        <Button label="Log Out" buttonStyle="px-12"></Button>
       </div>
     </div>
   );
