f13809c46bc354104685ff192e58666ef991d33e
Gayathri-Nakkeeran <gayathiri.nakkeeran@bounteous.com>
2024-04-24 14:18:33 +0530

    use component in client page

---
+++ apps/web/app/clients/[id]/page.tsx
@@ -0,0 +1,4 @@
+import { RecentOrderCarousel, RecentOrderData } from '@repo/ui'
+export default function Client(): JSX.Element {
+  return <RecentOrderCarousel recentOrderCardData={RecentOrderData} />
+}
